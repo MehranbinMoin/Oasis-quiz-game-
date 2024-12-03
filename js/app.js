605467f608d2e80d6936f432fe59bc6b3727a196
mehranbinmoin <mehranmoin@gmail.com>
2024-12-03 13:10:59 -0500

    Added pseudocode and working on html and css.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/* Peusdocode for my Oasis quiz game:
+
+1. The game will be a 5-question quiz on one of the UK's best bands of all time!
+2. You must get at least 4 questions correct in order to win the game.
+3. If you get 3 or more questions incorrect, you must start over from the beginning.
+4. Each question will have 4 multiple choices that you can choose from.
+5. Once you select your answer for each question, you'll have the option to move forward with a 'Next' button at the botton of your quiz container section.
+6. After you submit your answer for the fifth question, you'll be redirected to a new screen within the quiz container that states whether you lost the game or won the game. 
+    6a. If you won the game, you'll hear a song from Oasis.
+    6b. If you lose, the prompt will ask you to 'Start over' and you'll hear a song from Blur.
+
+*/
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
